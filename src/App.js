daaecfd162792e27e0c44e15e12b3eff0f58ca77
Valentin Harapu <aharapu2@yahoo.com>
2022-06-26 13:08:38 +0300

    Add arrays algo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { CredCardForm } from "./components/CredCardForm";
-import { DuplicatesAlgo } from "./components/DuplicatesAlgo/DuplicatesAlgo";
+import { DuplicatesAlgo } from "./components/DuplicatesAlgo";
+import { ArraysAlgo } from "./components/ArraysAlgo";
 
 function App() {
   return (
     <div className="main-page">
       <CredCardForm />
       <DuplicatesAlgo />
+      <ArraysAlgo />
     </div>
   );
 }
---
+++ src/components/ArraysAlgo/ArraysAlgo.jsx
@@ -0,0 +1,68 @@
+import { Paper, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+
+export function ArraysAlgo() {
+  const [string, setString] = useState("");
+
+  const result = runArraysAlgoOn(string);
+  return (
+    <Paper
+      elevation={5}
+      style={{
+        width: 400,
+        backgroundColor: "lightcyan",
+        padding: 20,
+      }}
+    >
+      <Typography>Arrays Algo</Typography>
+      <TextField
+        label="input"
+        value={string}
+        onChange={(e) => setString(e.target.value)}
+        multiline
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        disabled
+        label="result"
+        multiline
+        fullWidth
+        value={result}
+        margin="normal"
+      />
+      <Typography fontSize={12}>
+        All the computation issues discussed for string parsing apply to this
+        array parsing as well.
+      </Typography>
+    </Paper>
+  );
+}
+
+function runArraysAlgoOn(string = "") {
+  let arr = string.split(",");
+  const numbers = arr.map((n) => parseInt(n, 10));
+
+  let largest = 0;
+  let secondLargest = 0;
+
+  numbers.forEach((num) => {
+    const digits = String(num)
+      .split("")
+      .map((d) => parseInt(d, 10));
+    const sumOfDigits = digits.reduce((acc, d) => acc + d, 0);
+    if (sumOfDigits % 2 === 0) return;
+
+    if (num > largest) {
+      secondLargest = largest;
+      largest = num;
+      return;
+    }
+
+    if (num > secondLargest) {
+      secondLargest = num;
+    }
+  });
+
+  return largest + secondLargest;
+}
